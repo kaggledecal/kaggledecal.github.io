704af443071f84789899f76294e709a91b5c15dd
philkuz <superpker@gmail.com>
2016-09-06 15:10:23 -0700

    Refined the routes. Added a hello user/authentication version of the future authentication script.

--- client/routes.js
+++ client/routes.js
@@ -10,3 +10,35 @@ FlowRouter.route('/about', {
     BlazeLayout.render("aboutPage")
   }
 });
+FlowRouter.route('/login', {
+    name: 'login',
+    triggersEnter: [function() {
+        // if Meteor.
+    }],
+    action(params, queryParams) {
+        // if
+        BlazeLayout.render("loginPage")
+    }
+});
+
+const loggedIn = FlowRouter.group({
+  triggersEnter: [ function() {
+      // actions to take if user is not logged in
+      if (!(Meteor.loggingIn() || Meteor.userId())) {
+        let route = FlowRouter.current();
+        if (route.route.name !== 'login') {
+          Session.set('redirectAfterLogin', route.path);
+        }
+        return FlowRouter.go('login');
+      }
+    }
+  ]
+});
+
+loggedIn.route('/dashboard', {
+  name: 'dashboard',
+  action: function() {
+    return BlazeLayout.render("dashboardPage");
+  }
+});
+// ---
